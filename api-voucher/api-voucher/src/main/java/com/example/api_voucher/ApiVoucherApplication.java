8fef04d2d4ef55168d9e33e2aafb466dacd0bc9c
Gabriel Lorran <glorran0810@gmail.com>
2025-06-30 16:11:28 -0300

    Add files via upload

---
+++ api-voucher/api-voucher/src/main/java/com/example/api_voucher/ApiVoucherApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_voucher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVoucherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVoucherApplication.class, args);
+	}
+
+}
---
+++ api-voucher/api-voucher/src/test/java/com/example/api_voucher/ApiVoucherApplicationTests.java
@@ -0,0 +1,14 @@
+/*package com.example.api_voucher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVoucherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+ */
